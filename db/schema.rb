6855aac1af58e67d164651e01fe1d189a4750705
Joe Cochran <j_cochran@me.com>
2019-01-06 21:55:53 -0600

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_01_040426) do
+ActiveRecord::Schema.define(version: 2019_01_05_025433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
