204e5040cac0e639c13702d088583a92f9389ad6
Joe Cochran <j_cochran@me.com>
2019-01-14 00:07:53 -0600

    skeleton category page

--- app/javascript/components/Categories.js
+++ app/javascript/components/Categories.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { passCsrfToken } from '../util/helpers';
 
 class Categories extends Component {
   state = {
-
+    categories: [],
   };
 
   componentDidMount() {
     document.title = 'All Categories';
+    this.getCategories();
+  }
+
+  getCategories() {
+    axios.get('/api/categories').then((response) => {
+      this.setState({ categories: response.data.categories });
+    });
   }
 
   render() {
+    const { categories } = this.state;
     return (
       <div className="container mx-auto">
         <h1 className="mt-6 mb-6">Categories</h1>
+        <ul>
+          {categories.map(category => (
+            <li key={category.id}>{category.title}</li>
+          ))}
+        </ul>
+
       </div>
     );
   }
